23bc4a010764887341273323af9c3850f4bee386
Tomatos <3333331969@qq.com>
2025-08-02 11:19:38 +0800

    feat: 搭建项目脚架

---
+++ mall-mgt/src/main/java/com/mall/Application.java
@@ -0,0 +1,15 @@
+package com.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : Tomatos
+ * @date : 2025/8/2
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
