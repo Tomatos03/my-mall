74d9a22d4f1893978c1a99b38295542a918db802
Tomatos <3333331969@qq.com>
2025-08-28 15:10:26 +0800

    feat: 通过Api查询Ip所在城市

---
+++ mall-pojo/src/main/java/com/mall/dto/CityDTO.java
@@ -0,0 +1,39 @@
+package com.mall.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ *
+ * @author : Tomatos
+ * @date : 2025/8/28
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class CityDTO {
+
+    /**
+     * ip
+     */
+    private String ip;
+
+    /**
+     * 国家
+     */
+    private String country;
+
+    /**
+     * 省份
+     */
+    private String province;
+
+    /**
+     * 所在城市
+     */
+    private String city;
+}
