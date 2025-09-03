5975fbf243df650be726b7c3a7c7b179fc1f9ae4
Tomatos <3333331969@qq.com>
2025-09-03 15:37:55 +0800

    refractor: 添加自动配置注解, 简化配置书写

--- mall-common/src/main/java/com/mall/common/properties/CaptchaProperties.java
+++ mall-common/src/main/java/com/mall/common/properties/CaptchaProperties.java
@@ -2,7 +2,9 @@ package com.mall.common.properties;
 
 import com.mall.api.properties.CaptchaProps;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 
@@ -12,10 +14,10 @@ import org.springframework.stereotype.Component;
  * @author : Tomatos
  * @date : 2025/8/5
  */
-@Getter
+@Setter
 @Component
+@ConfigurationProperties("my-mall.captcha")
 public class CaptchaProperties implements CaptchaProps {
-    @Value("${my_mall.captcha.expiration}")
     private Long expiration;
 
     @Override
--- mall-common/src/main/java/com/mall/common/properties/JwtProperties.java
+++ mall-common/src/main/java/com/mall/common/properties/JwtProperties.java
@@ -2,7 +2,9 @@ package com.mall.common.properties;
 
 import com.mall.api.properties.JwtProps;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,10 +13,10 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Getter
+@Setter
+@ConfigurationProperties("my-mall.jwt")
 public class JwtProperties implements JwtProps {
-    @Value("${mall.web.security.privateKey:MIIBVgIBADANBgkqhkiG9w0BAQEFAASCAUAwggE8AgEAAkEAv5YziQe5zP5OAyVYfIh2fJDiJuEeD4ZGNpVQjiPvsNGhvwjF/SCDECMwZl1lYAL6wNDNNXhr/ILV5xF0ZHg0+wIDAQABAkBDQRjqO5oPyh3coNmFLepaJAofl9sTnBE9zACxRRA5Q2ErwtUVi+A4qLX+HPDbPFWCbvEPKsGods4BiAL+rxWxAiEA+W9p3bfu+JA2XTxEboH9Cd5/g92PZGALne+cVKAKfFcCIQDEoQebpFK/hw8AWvWRZ4vXZLoocv8qEhnPEySCqTtl/QIhAK0vdSLTdaGxh31+vci4ijcS/BhTeh7oLMiwuCttnzorAiEAouivvvokPN8PRMIX10KtD+Y6fizYz+hzTaeUhlTkC5kCIQCPDDmumEPGzUDmFe1hlL3AqUeCNNnVhxYQrI4bNHFZvQ==}")
     private String secret;
-    @Value("${my_mall.jwt.expiration}")
     private Long expiration;
 
     @Override
